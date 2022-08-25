0dbc3f231d703ec555d3f58b32691a9f5bf6eb26
JuanFigueroa21 <112037364+JuanFigueroa21@users.noreply.github.com>
2022-08-25 15:20:58 -0500

    Add files via upload

---
+++ IRPO Tarea 2, tabla_del_dos.py
@@ -0,0 +1,13 @@
+# -*- coding: iso-8859-15 -*-
+# Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+# Es posible que el codigo generado no sea completamente correcto. Si encuentra
+# errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
+
+if __name__ == '__main__':
+	x = int()
+	y1 = int()
+	for x in range(1,11):
+		y1 = x*2
+		print "2x",x,"=",y1
+
